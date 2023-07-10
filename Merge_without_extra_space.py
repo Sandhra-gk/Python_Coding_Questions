3366a003afb2af83469545ec666051ae2e221a66
Sandhra GireeshKumar <79519899+Sandhra-gk@users.noreply.github.com>
2023-07-10 16:17:30 +0530

    Update Merge_without_extra_space.py

--- Merge_without_extra_space.py
+++ Merge_without_extra_space.py
@@ -1,5 +1,5 @@
 # Given two sorted arrays arr1[] and arr2[] of sizes n and m in non-decreasing order. Merge them in sorted order without using any extra space. 
-Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
+# Modify arr1 so that it contains the first N elements and modify arr2 so that it contains the last M elements.
 
 N = int(input("Enter size of array 1: "))
 arr1 = []
