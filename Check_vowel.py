78edbe93723daf0f026d45109262e2ae3fd25b32
Sandhra GireeshKumar <79519899+Sandhra-gk@users.noreply.github.com>
2023-07-14 16:02:27 +0530

    Create Check_vowel.py

---
+++ Check_vowel.py
@@ -0,0 +1,9 @@
+# Check if entered alphabet is a vowel or not
+
+vowels = ['A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u']
+character = input("Enter character: ")
+
+if character in vowels:
+    print("Vowel")
+else:
+    print("Not a vowel")
