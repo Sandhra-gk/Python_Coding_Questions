1da8c33ef472cf24bc31b1865f1bca44bb4215c5
Sandhra GireeshKumar <79519899+Sandhra-gk@users.noreply.github.com>
2023-07-14 18:10:36 +0530

    Create No_of_digits.py

---
+++ No_of_digits.py
@@ -0,0 +1,5 @@
+# Take an integer as the input from the user and then calculate the number of digits in the given input and print it as the output.
+
+num = int(input("Enter integers: "))
+snum =str(num)
+print(len(snum))
